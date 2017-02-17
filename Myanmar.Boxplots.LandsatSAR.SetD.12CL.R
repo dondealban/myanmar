4e5b3bd93a3651fe180b18ea1f33531ca88fa25d
Don De Alban <dondealban@gmail.com>
2017-02-17 14:36:26 +0800

    Created script; inserted header comment section

---
+++ Myanmar.Boxplots.LandsatSAR.SetD.12CL.R
@@ -0,0 +1,12 @@
+# This R script generates box-whisker plots using ggplot2 package to visualise the
+# distribution of values for each predictor variable across 12 land cover types 
+# derived from combined Landsat and L-band SAR data covering Tanintharyi, Myanmar.
+#
+# Land cover types include: forest (FOR); mangrove (MNG); oil palm mature (OPM);
+# oil palm young (OPY); rubber mature (RBM); rubber young (RBY); rubber new (RBN);
+# shrub/orchard (SHB); rice paddy (RPD); built-up area (BUA); bare soil/ground (BSG);
+# and water (WTR).
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   17 Feb 2017
+# Last Modified:  
