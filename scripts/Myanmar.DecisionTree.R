790f22d41b746559942a6724fee0729159d160e0
Don De Alban <dondealban@gmail.com>
2017-02-17 15:16:40 +0800

    Created script for test decision tree classification

---
+++ scripts/Myanmar.DecisionTree.R
@@ -0,0 +1,16 @@
+# This R script implements a decision tree classification using the tree package. The
+# objective is to generate a tree based on specified predictor variables and to determine
+# the variables and thresholds used for splitting classes.
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   17 Feb 2017
+# Last Modified:  
+
+
+# LOAD LIBRARIES AND DATA
+
+# Set working directory
+setwd(""/Users/dondealban/Dropbox/Research/myanmar/"")
+
+# Load the required R libraries
+library(ggplot2)
