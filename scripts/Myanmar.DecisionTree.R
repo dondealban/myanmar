edcbc890c660d4d5c35b3f93c7a5f429a8adecea
Don De Alban <dondealban@gmail.com>
2017-02-17 15:58:29 +0800

    Defined classification levels into variables

--- scripts/Myanmar.DecisionTree.R
+++ scripts/Myanmar.DecisionTree.R
@@ -17,3 +17,17 @@ library(tree)
 
 # Read data, define variables, and store data in variables
 data <- read.csv(file="SetD_2015_LP_1D_30m_RF_12CL_ForR.csv", header=TRUE, sep=",")
+
+
+# CREATE SUBSETS TO DEFINE CLASSES IN MULTI-LEVEL CLASSIFICATION
+
+# Level 1: vegetation | non-vegetation
+data$LC1[data$LC_TYPE=="FOR" | data$LC_TYPE=="MNG" | data$LC_TYPE=="OPM" | data$LC_TYPE=="RBM" | data$LC_TYPE=="SHB"] <- "VEG"
+data$LC1[data$LC_TYPE!="FOR" & data$LC_TYPE!="MNG" & data$LC_TYPE!="OPM" & data$LC_TYPE!="RBM" & data$LC_TYPE!="SHB"] <- "NVG"
+
+# Level 2: forest | shrubs/orchards | mangrove/oil palm/rubber
+data$LC2[data$LC1=="NVG"] <- "NVG"
+data$LC2[data$LC1!="NVG" & data$LC_TYPE=="FOR"] <- "FOR"
+data$LC2[data$LC1!="NVG" & data$LC_TYPE=="SHB"] <- "SHB"
+# data$LC2[data$LC_TYPE=="SHB"] <- "FOR"
+
