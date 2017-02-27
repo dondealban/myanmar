1e47b996c0a4c01936df732fab5ee1972526492d
Don De Alban <dondealban@gmail.com>
2017-02-27 14:08:54 +0800

    Revised header comment section

--- scripts/Myanmar.Boxplots.LandsatSAR.SetA.R
+++ scripts/Myanmar.Boxplots.LandsatSAR.SetA.R
@@ -1,6 +1,9 @@
 # This R script generates box-whisker plots using ggplot2 package to visualise the
 # distribution of values for each predictor variable across 9 land cover types 
 # derived from combined Landsat and L-band SAR data covering Tanintharyi, Myanmar.
+# This script plots SetA values consisting of 1995 and 2015 data from Landsat-5
+# (7-8 bands, 5 indices) and JERS-1 or ALOS/PALSAR-2 (1 polarisation and 8 GLCM
+# textures).
 #
 # Land cover types include: forest (FOR); mangrove (MNG); oil palm mature (OPM);
 # rubber mature (RBM); shrub/orchard (SHB); rice paddy (RPD); built-up area (BUA);
