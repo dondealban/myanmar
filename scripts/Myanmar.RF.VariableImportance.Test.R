b4c6deb581df17fface07c5f52615d11f0c3ec77
Don De Alban <dondealban@gmail.com>
2017-03-03 08:38:17 +0800

    Created script for Random Forest variable importance test

---
+++ scripts/Myanmar.RF.VariableImportance.Test.R
@@ -0,0 +1,10 @@
+# This R script implements a decision tree classification using the tree package. The
+# objective is to generate a tree based on specified predictor variables and to determine
+# the variables and thresholds used for splitting classes.
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   17 Feb 2017
+# Last Modified:  20 Feb 2017
+
+
+# LOAD LIBRARIES AND DATA
