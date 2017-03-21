2baa1a1724134a5dc180352346fa8c9d5abc6ae2
Don De Alban <dondealban@gmail.com>
2017-03-21 15:38:18 +0800

    Created script for computing McNemar's test statistic

---
+++ scripts/Myanmar.McNemarTest.R
@@ -0,0 +1,6 @@
+# This script calculates the McNemar's test statistic to evaluate the statistical
+# significance of the differences between the classification results.  
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   20 Mar 2017
+# Last Modified:  
