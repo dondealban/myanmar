169679bc08c78c9705615e06c5c5d025171e9dad
Don De Alban <dondealban@gmail.com>
2017-03-21 16:50:38 +0800

    Inserted working directory, test contingency tables

--- scripts/Myanmar.McNemarTest.R
+++ scripts/Myanmar.McNemarTest.R
@@ -4,3 +4,20 @@
 # Script By:      Jose Don T De Alban
 # Date Created:   20 Mar 2017
 # Last Modified:  
+
+
+# LOAD LIBRARIES AND DATA
+
+# Set working directory
+setwd("/Users/dondealban/Dropbox/Research/myanmar/mcnemar test/")
+
+
+# CREATE 2x2 CONTINGENCY MATRICES
+# Note: value taken from separate spreadsheets
+
+# SetA 1995: Landsat+SAR vs. Landsat
+mat1 = matrix(c(756,42,5,38), ncol=2, byrow=TRUE)
+
+
+
+
