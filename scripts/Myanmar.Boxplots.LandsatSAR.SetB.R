47f84539544266d1e36648a3c10978a0a268f1a7
Don De Alban <dondealban@gmail.com>
2017-04-17 12:15:49 +0800

    Copied csv data file, revised filename path

--- scripts/Myanmar.Boxplots.LandsatSAR.SetB.R
+++ scripts/Myanmar.Boxplots.LandsatSAR.SetB.R
@@ -21,7 +21,7 @@ setwd("/Users/dondealban/Dropbox/Research/myanmar/image statistics/distribution/
 library(ggplot2)
 
 # Read data, define variables, and store data in variables
-data <- read.csv(file="Table_SetB_2015_LP_1D_30m_RF_ForR.csv", header=TRUE, sep=",")
+data <- read.csv(file="Table_SetB_2015_LP_30m_RF_ForR.csv", header=TRUE, sep=",")
 
 # GENERATE PLOTS
 # Generate boxplots of land cover types for each predictor variable
