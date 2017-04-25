56a66a99440ede244827af789e2c5b158079f1cc
Don De Alban <dondealban@gmail.com>
2017-04-25 22:29:04 +0800

    Initial commit of script generating Sankey diagram

---
+++ scripts/Myanmar.IntensityAnalysis.Sankey.SetA.R
@@ -0,0 +1,13 @@
+# This R script generates a Sankey diagram using riverplot package to visualise the
+# land use transitions from the change analysis between 9 land cover types derived from
+# combined Landsat and L-band SAR data covering Tanintharyi, Myanmar. This script plots
+# SetA values consisting of 1995 and 2015 data from Landsat-5 (7 bands, 5 indices) and
+# JERS-1 or ALOS/PALSAR-2 (1 polarisation and 8 GLCM textures).
+#
+# Land cover types include: forest (FOR); mangrove (MNG); oil palm mature (OPM);
+# rubber mature (RBM); shrub/orchard (SHB); rice paddy (RPD); built-up area (BUA);
+# bare soil/ground (BSG); and water (WTR).
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   25 Apr 2017
+# Last Modified:
