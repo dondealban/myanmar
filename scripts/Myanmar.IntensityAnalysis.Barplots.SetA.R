48520b0d702a86349661613912271f30a97bac29
Don De Alban <dondealban@gmail.com>
2017-04-25 18:28:06 +0800

    Revised script with test category level barplot, saved outputs

--- scripts/Myanmar.IntensityAnalysis.Barplots.SetA.R
+++ scripts/Myanmar.IntensityAnalysis.Barplots.SetA.R
@@ -23,13 +23,31 @@ library(ggplot2)
 library(plyr)
 
 # Read data, define variables, and store data in variables
+
+# Category level
+tniALL <- read.csv(file="Change_TNI_SetA_1995_2015_ALL_CATEGORY.csv", header=TRUE, sep=",")
+
+# Transition level
 tniFOR <- read.csv(file="Change_TNI_SetA_1995_2015_FOR.csv", header=TRUE, sep=",")
+tniMNG <- read.csv(file="Change_TNI_SetA_1995_2015_MNG.csv", header=TRUE, sep=",")
 
 
 # MELT AND SUBSET DATA
+
+# Category Level
+meltALL <- melt(tniALL, id.vars="Categories")
+lossALL <- subset(meltALL, variable=="Loss.Intensity")
+gainALL <- subset(meltALL, variable=="Gain.Intensity")
+
+# Forest
 mtFOR <- melt(tniFOR, id.vars="Category.Transitions")
 frFOR <- subset(mtFOR, variable=="From.FOR")
 toFOR <- subset(mtFOR, variable=="To.FOR")
+
+# Mangrove
+mtMNG <- melt(tniMNG, id.vars="Category.Transitions")
+frMNG <- subset(mtMNG, variable=="From.MNG")
+toMNG <- subset(mtMNG, variable=="To.MNG")
 
 
 # GENERATE PLOTS
@@ -37,20 +55,30 @@ toFOR <- subset(mtFOR, variable=="To.FOR")
 
 # Forest
 intFOR <- ggplot() + geom_bar(data=toFOR, aes(x=Category.Transitions, y=value, fill="#8ACD66"),  stat="identity")
-intFOR <- intFOR   + geom_bar(data=frFOR, aes(x=Category.Transitions, y=-value, fill="#C9400E"), stat="identity")
+intFOR <- intFOR   + geom_bar(data=frFOR, aes(x=Category.Transitions, y=-value, fill="#B43507"), stat="identity")
 intFOR <- intFOR   + geom_hline(yintercept=0, colour="grey90")
-intFOR <- intFOR   + geom_hline(yintercept=-1.03, linetype="dashed", colour="gray30") # FROM uniform line
-intFOR <- intFOR   + geom_hline(yintercept=0.52, linetype="dashed", colour="gray30") # TO uniform line
-intFOR <- intFOR   + geom_text() + annotate("text", label="uniform loss", x=1, y=-1.13, colour="gray30")
-intFOR <- intFOR   + geom_text() + annotate("text", label="uniform gain", x=1, y=0.62, colour="gray30")
+intFOR <- intFOR   + geom_hline(aes(yintercept=0.52, colour="#009404"), linetype="dashed") # TO uniform line
+intFOR <- intFOR   + geom_hline(aes(yintercept=-1.03, colour="#FF0000"), linetype="dashed") # FROM uniform line
 intFOR <- intFOR   + labs(title="Transition Intensity for Forest", x="Transition Category", y="Transition Intensity (% of Category)")
-intFOR <- intFOR   + scale_fill_manual(values=c("#8ACD66", "#C9400E"), name="", labels = c("FROM Forest", "TO Forest"))
+intFOR <- intFOR   + scale_fill_manual(values=c("#8ACD66", "#B43507"), name="Change Direction", labels = c("TO Forest", "FROM Forest"))
+intFOR <- intFOR   + scale_colour_manual(values=c("#009404", "#FF0000"), name="Uniform Line", labels = c("Gain", "Loss"))
 intFOR <- intFOR   + theme_minimal()
 
-
+# Mangrove
+intMNG <- ggplot() + geom_bar(data=toMNG, aes(x=Category.Transitions, y=value, fill="#8ACD66"),  stat="identity")
+intMNG <- intMNG   + geom_bar(data=frMNG, aes(x=Category.Transitions, y=-value, fill="#B43507"), stat="identity")
+intMNG <- intMNG   + geom_hline(yintercept=0, colour="grey90")
+intMNG <- intMNG   + geom_hline(aes(yintercept=0.07, colour="#009404"), linetype="dashed") # TO uniform line
+intMNG <- intMNG   + geom_hline(aes(yintercept=-0.10, colour="#FF0000"), linetype="dashed") # FROM uniform line
+intMNG <- intMNG   + labs(title="Transition Intensity for Mangrove", x="Transition Category", y="Transition Intensity (% of Category)")
+intMNG <- intMNG   + scale_fill_manual(values=c("#8ACD66", "#B43507"), name="Change Direction", labels = c("TO Mangrove", "FROM Mangrove"))
+intMNG <- intMNG   + scale_colour_manual(values=c("#009404", "#FF0000"), name="Uniform Line", labels = c("Gain", "Loss"))
+intMNG <- intMNG   + theme_minimal()
 
 
 # OUTPUT PLOTS
 # Output boxplots to a PNG file
 
 ggsave(intFOR, file="IntensityAnalysis-FOR.pdf", width=19.89, height=15, units="cm", dpi=300)
+ggsave(intMNG, file="IntensityAnalysis-MNG.pdf", width=19.89, height=15, units="cm", dpi=300)
+
