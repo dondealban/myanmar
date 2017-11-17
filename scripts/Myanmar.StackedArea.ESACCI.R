3d83cee041f4b932b0f777510b523848d4a99d71
dondealban <dondealban@gmail.com>
2017-11-17 14:39:52 +0800

    Uploaded csv data files; load libraries and data

--- scripts/Myanmar.StackedArea.ESACCI.R
+++ scripts/Myanmar.StackedArea.ESACCI.R
@@ -1,2 +1,17 @@
 # Script Description --------------------
 # This script creates a stacked area graph of time-series land cover data. 
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   17 Nov 2017
+# Last Modified:  
+
+
+# Set Working Directory -----------------
+setwd("/Users/dondealban/Dropbox/Research/myanmar/intensity analysis/stacked area/ipcc/")
+
+# Load Libraries and Data ---------------
+library(ggplot2)
+
+
+# Read data, define variables, and store data in variables
+c1992 <- read.csv(file="Landscape_1992_Reclass.csv", header=TRUE, sep=",")
