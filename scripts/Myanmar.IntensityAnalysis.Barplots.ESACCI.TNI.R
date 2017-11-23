a784ad8f69bd0d34c11ba01b9629a3090109c94a
dondealban <dondealban@gmail.com>
2017-11-23 14:48:42 +0800

    Revised header comment section of script

---
+++ scripts/Myanmar.IntensityAnalysis.Barplots.ESACCI.TNI.R
@@ -0,0 +1,13 @@
+# This R script generates barlots using ggplot2 package to visualise the results of
+# intensity analysis of changes between 4 land cover types derived from ESA CCI land
+# cover data (1992-2015) covering Tanintharyi, Myanmar. The original land cover classes
+# were reclassified into four bespoke classes: Forest (FOR), Other Vegetation (VEG),
+# Cropland (CRP), and Non-Forest (NON).
+#
+# Intensity analysis was calculated using Dinamica EGO platform using the 24 annual 
+# ESA CCI land cover data layers. The domain of the analysis is the landscape extent
+# within the following coordinates: 97 to 100 deg E longitude; 9 to 16 deg N latitude.
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   25 Apr 2017
+# Last Modified:  23 Nov 2017
