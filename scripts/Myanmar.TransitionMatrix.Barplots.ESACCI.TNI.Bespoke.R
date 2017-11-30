a1ca139ee61c26d37138d129e1e180ff76ed6bac
dondealban <dondealban@gmail.com>
2017-11-30 21:37:50 +0800

    Saved changes to script

--- scripts/Myanmar.TransitionMatrix.Barplots.ESACCI.TNI.Bespoke.R
+++ scripts/Myanmar.TransitionMatrix.Barplots.ESACCI.TNI.Bespoke.R
@@ -16,69 +16,32 @@ library(ggplot2)
 library(reshape2)
 
 
-# Set 01 --------------------------------
-
-# Set working directory
-setwd("/Users/dondealban/Dropbox/Research/myanmar/simulation/validation/tanintharyi/bespoke/set 01/")
-
-# Read CSV data file
-dataSet1 <- read.csv(file="tni_multiple_windows_similarity_sub_1992_2015.csv", header=TRUE, sep=",")
-colnames(dataSet1) <- c("Window.Size","Min.Sim","Max.Sim") # Rename column names
-dfSet1 <- melt(dataSet1, id.vars="Window.Size", na.rm=TRUE)
-
-# Create line graphs
-plotSet1 <- ggplot() + geom_line(data=dfSet1, aes(x=Window.Size, y=value, colour=variable))
-plotSet1 <- plotSet1 + labs(title="Observed vs Simulated Map Similarity", 
-                            subtitle="Single time interval from 1992 to 2015",
-                            x="Window Size", y="% Similarity (x 100)")
-plotSet1 <- plotSet1 + scale_colour_manual(values=c("#b3cce6","#264d73"), name="Similarity", labels = c("Minimum","Maximum"))
-plotSet1 <- plotSet1 + ylim(0,1) + xlim(0.0,11)
-plotSet1 <- plotSet1 + theme_light()
-
-
 # Set 02 --------------------------------
 
 # Set working directory
-setwd("/Users/dondealban/Dropbox/Research/myanmar/simulation/validation/tanintharyi/bespoke/set 02/similarity/")
+setwd("/Users/dondealban/Dropbox/Research/myanmar/simulation/validation/tanintharyi/bespoke/set 02/transition matrix/")
 
 # Read csv files in the directory and store as a list
-files <- list.files()
+tmatrices <- list.files()
 
 # Function to read maximum similarity data
-readmax <- function(filename) {
+readdata <- function(filename) {
   df <- read.csv(filename, sep=",")
-  max <- df[, 3]           # Read column with maximum similarity values
-  names(max) <- df[, 1]    # Read column with window sizes
-  return(max)
+  vec <- df[, 3]           # Read column with maximum similarity values
+  names(vec) <- df[, 1]    # Read column with window sizes
+  return(vec)
 }
 
 # Combine as window sizes and similarity values in a matrix
-maxSet2 <- do.call(rbind, lapply(files, readmax))
+tmSet2 <- do.call(rbind, lapply(tmatrices, readdata))
 
 # Add years as another column
-row.names(maxSet2) <- c("1992-1995","1993-1996","1994-1997","1995-1998","1996-1999",
-                        "1997-2000","1998-2001","1999-2002","2000-2003","2001-2004",
-                        "2002-2005","2003-2006","2004-2007","2005-2008","2006-2009",
-                        "2007-2010","2008-2011","2009-2012","2010-2013","2011-2014",
-                        "2012-2015")
+row.names(tmSet2) <- c("1992-1995","1993-1996","1994-1997","1995-1998","1996-1999",
+                       "1997-2000","1998-2001","1999-2002","2000-2003","2001-2004",
+                       "2002-2005","2003-2006","2004-2007","2005-2008","2006-2009",
+                       "2007-2010","2008-2011","2009-2012","2010-2013","2011-2014",
+                       "2012-2015")
 
-# Function to read minimum similarity data
-readmin <- function(filename) {
-  df <- read.csv(filename, sep=",")
-  min <- df[, 2]           # Read column with minimum similarity values
-  names(min) <- df[, 1]    # Read column with window sizes
-  return(min)
-}
-
-# Combine as window sizes and similarity values in a matrix
-minSet2 <- do.call(rbind, lapply(files, readmin))
-
-# Add years as another column
-row.names(minSet2) <- c("1992-1995","1993-1996","1994-1997","1995-1998","1996-1999",
-                        "1997-2000","1998-2001","1999-2002","2000-2003","2001-2004",
-                        "2002-2005","2003-2006","2004-2007","2005-2008","2006-2009",
-                        "2007-2010","2008-2011","2009-2012","2010-2013","2011-2014",
-                        "2012-2015")
 
 # Convert wide format data frame into long format data frame
 mxSet2 <- melt(maxSet2, id.vars="years", variable.name="windowsize", value.name="percentage")
