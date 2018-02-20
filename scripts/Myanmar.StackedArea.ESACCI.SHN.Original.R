c8c2ceb2b00520a983a5d12304e0219dc59fb888
dondealban <dondealban@gmail.com>
2018-02-20 14:26:58 +0800

    Changed column order/numbering

--- scripts/Myanmar.StackedArea.ESACCI.SHN.Original.R
+++ scripts/Myanmar.StackedArea.ESACCI.SHN.Original.R
@@ -29,9 +29,9 @@ readdata <- function(filename) {
 # Combine as class codes and percentage values in a matrix
 temp <- do.call(rbind, lapply(filenames, readdata))
 colnames(temp) <- c("1","2","3","4","5",
-                    "13","14","15","16",
-                    "6","7","8","9","10","11","12",
-                    "17","18","19","20","21")
+                    "12","13","14","15",
+                    "6","7","8","9","10","11",
+                    "16","17","18","19")
 
 # Add years as another column
 row.names(temp) <- c("1992","1993","1994","1995","1996","1997","1998","1999",
