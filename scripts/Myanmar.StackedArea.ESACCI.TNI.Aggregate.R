d615f9f2337d228e855dc079c1d39c4e6fca2e32
dondealban <dondealban@gmail.com>
2018-03-08 13:08:18 +0800

    Revised class order and hex colors in plotting parameters

--- scripts/Myanmar.StackedArea.ESACCI.TNI.Aggregate.R
+++ scripts/Myanmar.StackedArea.ESACCI.TNI.Aggregate.R
@@ -29,7 +29,7 @@ readdata <- function(filename) {
 
 # Combine as class codes and percentage values in a matrix
 temp <- do.call(rbind, lapply(filenames, readdata))
-colnames(temp) <- c("3","5","4","2","6","1")
+colnames(temp) <- c("4","2","3","5","1","6")
 
 # Add years as another column
 row.names(temp) <- c("1992","1993","1994","1995","1996","1997","1998","1999",
@@ -41,9 +41,8 @@ data <- melt(temp, id.vars="years", variable.name="class", value.name="percentag
 colnames(data) <- c("Years","Class","Percentage")
 
 # Create Stacked Area Graphs ------------
-aggreg <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class, labels=c("Cropland","Other Vegetation","Forest","Non-Vegetation"))), data=data)
+aggreg <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class, labels=c("Other Vegetation","Mosaic Vegetation","Shrubland","Cropland","Forest","Non-Vegetation"))), data=data)
 aggreg <- aggreg + labs(title="Land Cover Transitions", x="Year", y="Percentage of Landscape", fill="Land Cover")
-aggreg <- aggreg + theme_bw() +scale_fill_manual(values=c("#ffff64","#8ca000","#003c00","#a6cee3"))
+aggreg <- aggreg + theme_bw() +scale_fill_manual(values=c("#ffb432","#8ca000","#966400","#ffff64","#003c00","#a6cee3"))
 
 ggsave(aggreg, file="StackedArea-Aggregate.pdf", width=19.89, height=15, units="cm", dpi=300)
-
