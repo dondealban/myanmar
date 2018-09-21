5bfd2e6d66409538b471b01e568b0ac2c0d6a3d7
j-johanness <j-johanness@users.noreply.github.com>
2018-09-21 14:38:09 +0800

    Starting on the R script within Atom

---
+++ 3 mmr land cover transitions/4. Sankey diagrams/R script to arrange sankeys.R
@@ -0,0 +1,19 @@
+# Creator: J Johanness
+
+# Date: 18-09-2018
+
+# Purpose: This is to experiment with creating and commiting an R script to test if
+# its changes will be documented by GitHub, GitHub Desktop, and Atom
+# and which of these can we manipulate the R Script directly (not R Markdown, unless they are interchangeably linked in some way)
+
+# Firstly, this Script was done via Ctrl + Shift + N (New R Script), which I believe once I save, I will need to Commit and Push to the repo
+
+# Script Example
+
+# This test/experimentation for R Scripts for GitHub, will be done for the calculation of the proportions for each change from the QGIS SCP LCCR .csv's to generate a stacked area chart to see which transitions contributed the most to overall change in the landscape
+
+
+
+# Start
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - -
