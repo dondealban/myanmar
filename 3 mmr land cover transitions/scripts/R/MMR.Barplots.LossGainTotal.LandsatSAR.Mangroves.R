0e4dc4b81a04a6cabda962d86cd163de50cd7339
dondealban <dondealban@gmail.com>
2018-12-13 14:15:19 +0800

    Revised ggplot2 plotting parameters

--- 3 mmr land cover transitions/scripts/R/MMR.Barplots.LossGainTotal.LandsatSAR.Mangroves.R
+++ 3 mmr land cover transitions/scripts/R/MMR.Barplots.LossGainTotal.LandsatSAR.Mangroves.R
@@ -14,5 +14,15 @@ setwd("/Users/dondealban/Dropbox/Research/myanmar/3 mmr land cover transitions/m
 library(tidyverse)
 
 # Read Input Data -------------------------
-csv <- read.csv(file="Loss & Gain Totals.csv", header=TRUE, sep=",")
+dfGROSS <- read.csv(file="Loss & Gain Totals.csv", header=TRUE, sep=",")
+
+# Generate Plots --------------------------
+plot <- ggplot() + geom_bar(data=dfGROSS, aes(x=Change, y=AreaKm2, fill=Category), stat="identity")
+plot <- plot + facet_wrap(~Interval)
+plot <- plot + labs(x="Mangrove Change per Time-Interval", y="Areal Extent (sq.km)")
+plot <- plot + scale_fill_manual(values=c("#ffff66","#ff0000","#246a24","#ff8000",
+                                          "#a65400","#ff00ff","#ccff66","#66ccff"))
+
+
+
 
