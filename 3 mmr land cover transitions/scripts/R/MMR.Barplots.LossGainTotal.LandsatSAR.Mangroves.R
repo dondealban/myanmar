39fcf034e43f5bad6b047aaa7e6436e69d91abc0
dondealban <dondealban@gmail.com>
2018-12-13 14:22:55 +0800

    Revised ggplot2 plotting parameters

--- 3 mmr land cover transitions/scripts/R/MMR.Barplots.LossGainTotal.LandsatSAR.Mangroves.R
+++ 3 mmr land cover transitions/scripts/R/MMR.Barplots.LossGainTotal.LandsatSAR.Mangroves.R
@@ -19,10 +19,13 @@ dfGROSS <- read.csv(file="Loss & Gain Totals.csv", header=TRUE, sep=",")
 # Generate Plots --------------------------
 plot <- ggplot() + geom_bar(data=dfGROSS, aes(x=Change, y=AreaKm2, fill=Category), stat="identity")
 plot <- plot + facet_wrap(~Interval)
-plot <- plot + labs(x="Mangrove Change per Time-Interval", y="Areal Extent (sq.km)")
+plot <- plot + labs(x="Mangrove Change per Time-Interval", y="Areal Extent (sq.km)", fill="Category")
 plot <- plot + scale_fill_manual(values=c("#ffff66","#ff0000","#246a24","#ff8000",
-                                          "#a65400","#ff00ff","#ccff66","#66ccff"))
+                                          "#ff00ff","#a65400","#ccff66","#66ccff"))
 
+# Save Output Plots -----------------------
+
+ggsave(plot, file="Barplot-Total-Gross-Loss-Gain-Mangroves.pdf", width=19.89, height=15, units="cm", dpi=300)
 
 
 
