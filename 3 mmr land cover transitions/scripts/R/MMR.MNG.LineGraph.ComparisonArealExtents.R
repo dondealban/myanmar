a389b7e24089c45c83b5ccba163c0f384251e118
dondealban <dondealban@gmail.com>
2019-01-15 10:05:13 +0800

    Initial commoit of new script

---
+++ 3 mmr land cover transitions/scripts/R/MMR.MNG.LineGraph.ComparisonArealExtents.R
@@ -0,0 +1,14 @@
+# Script Description --------------------
+# This R script generates line graphs using tidyverse package to visualise estimates of
+# areal extents of mangroves at specific time-points in Myanmar and its sub-national units
+# reported from published studies.
+#
+# Script By:      Jose Don T De Alban
+# Date Created:   15 Jan 2019
+# Last Modified:  
+
+# Set Working Directory -------------------
+setwd("/Users/dondealban/Dropbox/Research/myanmar/3 mmr land cover transitions/mangroves/")
+
+# Load Libraries --------------------------
+library(tidyverse)
