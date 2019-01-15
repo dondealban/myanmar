b714da8ccb222e10b066df3e7c360d1c7c6a15c0
dondealban <dondealban@gmail.com>
2019-01-15 10:56:14 +0800

    Inserted lines to generate two versions of linegraph plots

--- 3 mmr land cover transitions/scripts/R/MMR.MNG.LineGraph.ComparisonArealExtents.R
+++ 3 mmr land cover transitions/scripts/R/MMR.MNG.LineGraph.ComparisonArealExtents.R
@@ -15,3 +15,24 @@ library(tidyverse)
 
 # Read Input Data -------------------------
 dfAREAL <- read.csv(file="Areal_Extents_Comparison.csv", header=TRUE, sep=",")
+
+# Reorder Factor Levels -------------------
+dfAREAL$Study_Area_Rev <- factor(dfAREAL$Study_Area, levels=c("Ayeyarwady",
+                                                              "Bago",
+                                                              "Mon",
+                                                              "Rakhine",
+                                                              "Tanintharyi",
+                                                              "Yangon",
+                                                              "Myanmar")) 
+
+# Generate Plots --------------------------
+# Plot #1: Same y-scales
+plot1 <- ggplot() + geom_line(data=dfAREAL, aes(x=Time_Point, y=Areal_Extent_Km2, color=Reference), stat="identity")
+plot1 <- plot1 + facet_wrap(~Study_Area_Rev)
+plot1 <- plot1 + labs(x="Year", y="Areal Extent (sq.km)")
+# Plot #2: Different y-scales
+plot2 <- ggplot() + geom_line(data=dfAREAL, aes(x=Time_Point, y=Areal_Extent_Km2, color=Reference), stat="identity")
+plot2 <- plot2 + facet_wrap(~Study_Area_Rev, scales="free")
+plot2 <- plot2 + labs(x="Year", y="Areal Extent (sq.km)")
+
+
