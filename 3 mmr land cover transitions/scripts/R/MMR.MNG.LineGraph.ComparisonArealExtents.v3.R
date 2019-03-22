41eee924266679d4f86cc0041ad5c2d7ccc9a6e7
dondealban <dondealban@gmail.com>
2019-03-22 15:45:41 +0800

    Revised plotting parameters; inserted combined plot

--- 3 mmr land cover transitions/scripts/R/MMR.MNG.LineGraph.ComparisonArealExtents.v3.R
+++ 3 mmr land cover transitions/scripts/R/MMR.MNG.LineGraph.ComparisonArealExtents.v3.R
@@ -49,14 +49,21 @@ dfSUB2 <- dfAREAL %>% filter(Study_Area %in% c("Ayeyarwady","Rakhine","Taninthar
 dfSUB3 <- dfAREAL %>% filter(Study_Area %in% c("Bago","Mon","Yangon"))
 
 # Define Plots ----------------------------
+
 # Plot #1: Myanmar
 plot1 <- ggplot() + geom_line(data=dfSUB1, aes(x=Time_Point, y=Areal_Extent_Km2, color=Reference), stat="identity")
+plot1 <- plot1 + facet_wrap(~Study_Area)
+plot1 <- plot1 + theme(legend.position="none", axis.title.x=element_blank()) + labs(y=" ")
+
 # Plot #2: Extensive mangrove regions
 plot2 <- ggplot() + geom_line(data=dfSUB2, aes(x=Time_Point, y=Areal_Extent_Km2, color=Reference), stat="identity")
-plot2 <- plot2 + facet_wrap(~Study_Area, nrow=1) + theme(legend.position="none")
+plot2 <- plot2 + facet_wrap(~Study_Area, nrow=1) 
+plot2 <- plot2 + theme(legend.position="none", axis.title.x=element_blank()) + labs(y="Areal Extent (sq.km)")
+
 # Plot #3: Non-extensive mangrove regions
 plot3 <- ggplot() + geom_line(data=dfSUB3, aes(x=Time_Point, y=Areal_Extent_Km2, color=Reference), stat="identity")
-plot3 <- plot3 + facet_wrap(~Study_Area, nrow=1) + theme(legend.position="none")
+plot3 <- plot3 + facet_wrap(~Study_Area, nrow=1)
+plot3 <- plot3 + theme(legend.position="none") + labs(x="Year", y=" ")
 
 # Expose ggplot2 Layouts ------------------
 plotlayout <- lapply(list(plot1, plot2, plot3), expose_layout, FALSE, FALSE)
@@ -68,18 +75,10 @@ grid.arrange(
                         c(3))
 )
 
-
-
-
 # Generate Plots --------------------------
-# Plot #1: Same y-scales
-plot1 <- ggplot() + geom_line(data=dfAREAL, aes(x=Time_Point, y=Areal_Extent_Km2, color=Reference), stat="identity")
-plot1 <- plot1 + facet_wrap(~Study_Area_Rev)
-plot1 <- plot1 + labs(x="Year", y="Areal Extent (sq.km)")
-# Plot #2: Different y-scales
-plot2 <- ggplot() + geom_line(data=dfAREAL, aes(x=Time_Point, y=Areal_Extent_Km2, color=Reference), stat="identity")
-plot2 <- plot2 + facet_wrap(~Study_Area_Rev, scales="free")
-plot2 <- plot2 + labs(x="Year", y="Areal Extent (sq.km)")
+ggarrange(plot1, plot2, plot3)
+
+
 
 # Save Output Plots -----------------------
 ggsave(plot1, file="Linegraph-Areal-Extent-Comparison-Mangroves-SameScale.pdf", width=19.89, height=15, units="cm", dpi=300)
