9fb0ba6241c74fa2f5749cd0b4eac9246d2e4b16
dondealban <dondealban@gmail.com>
2019-05-09 12:32:15 +0800

    Revised BAG stacked area plot and script

--- 3 mmr land cover transitions/scripts/R/MMR.StackedArea.LandsatSAR.02.Bago.R
+++ 3 mmr land cover transitions/scripts/R/MMR.StackedArea.LandsatSAR.02.Bago.R
@@ -7,7 +7,7 @@
 #
 # Script By:      Jose Don T De Alban
 # Date Created:   20 Nov 2017
-# Last Modified:  04 Oct 2018
+# Last Modified:  09 May 2019
 
 
 # Set Working Directory -----------------
@@ -68,10 +68,10 @@ plot <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
                    data=data)
 plot <- plot + labs(title="Net Land Cover Transitions: Bago Region", x="Year", y="Percentage of Landscape", fill="Land Cover Category")
 plot <- plot + guides(fill=guide_legend(ncol=1))
-plot <- plot + theme(legend.position="bottom")
 plot <- plot + theme_bw()
 plot <- plot + scale_fill_manual(values=c("#ff0000","#246a24","#a6cee3","#6666ff","#ff8000",
                                           "#a65400","#ff00ff","#ccff66","#66ccff","#ffff66"))
 plot <- plot + scale_x_continuous(breaks=c(1996,2007,2016))
+plot <- plot + theme(legend.position="none")
 
-ggsave(plot, file="StackedArea-02-Bago.pdf", width=19.89, height=15, units="cm", dpi=300)
+ggsave(plot, file="StackedArea-02-Bago.pdf", width=16, height=15, units="cm", dpi=300)
