0be4f5ae5bf67014d3f7474e027a29838b83ead1
dondealban <dondealban@gmail.com>
2019-05-13 15:00:54 +0800

    Revised required packages; plotting parameters and saving output files

--- 3 mmr land cover transitions/scripts/R/MMR.MNG.StackedArea.GrossNetExtentsChanges.00.MMR.R
+++ 3 mmr land cover transitions/scripts/R/MMR.MNG.StackedArea.GrossNetExtentsChanges.00.MMR.R
@@ -14,7 +14,6 @@ library(reshape2)
 library(dplyr)
 library(ggplot2)
 library(egg)
-library(grid)
 
 # Read Input Data -------------------------
 dfRAW <- read.csv(file="ForR_Data_Split-Stacked-Area-Plots.csv", header=TRUE, sep=",")
@@ -31,19 +30,10 @@ dfMMR1 <- dfMMR %>% filter((TimeInterval %in% "1") & Change != "Gross Loss" & Ch
 dfMMR2 <- dfMMR %>% filter((TimeInterval %in% "2") & Change != "Gross Loss" & Change != "Undisturbed")
 
 # Define Polygons -------------------------
-
-
 dfGL1 <- data.frame(x=c(1996,2007,2007), y=c(12380.8662, 12380.8662, 6206.2371))  # gross loss 1996-2007
 dfGL2 <- data.frame(x=c(2007,2016,2016), y=c( 7438.2768,  7438.2768, 5168.6226))  # gross loss 2007-2016
 dfUN1 <- data.frame(x=c(1996,1996,2007,2007), y=c(0,4646.2970,4646.2970,0))       # undisturbed mangroves
 dfUN2 <- data.frame(x=c(2007,2007,2016,2016), y=c(0,4646.2970,4646.2970,0))       # undisturbed mangroves
-
-dfAYE <- mDATA %>% filter(Region %in% "Ayeyarwady")
-dfBAG <- mDATA %>% filter(Region %in% "Bago")
-dfMON <- mDATA %>% filter(Region %in% "Mon")
-dfRAK <- mDATA %>% filter(Region %in% "Rakhine")
-dfTNI <- mDATA %>% filter(Region %in% "Tanintharyi")
-dfYGN <- mDATA %>% filter(Region %in% "Yangon")
 
 # Define Plots ----------------------------
 
@@ -76,7 +66,7 @@ pMMR2 <- pMMR2 + scale_fill_manual(values=c("#ff0000","#00b050","#6666ff"))
 pMMR2 <- pMMR2 + guides(colour=guide_legend(reverse=TRUE))
 pMMR2 <- pMMR2 + labs(x="Interval 2", fill=element_blank(), colour="")
 pMMR2 <- pMMR2 + scale_x_continuous(breaks=c(2007,2016))
-pMMR2 <- pMMR2 + scale_y_continuous(limits=c(0,12700), breaks=c(2500,5000,7500,10000,12500), position="right")
+pMMR2 <- pMMR2 + scale_y_continuous(limits=c(0,12700), breaks=c(0,2500,5000,7500,10000,12500), position="right")
 pMMR2 <- pMMR2 + theme_light() + theme(axis.title.y=element_blank())
 
 # Expose ggplot2 Layouts ------------------
@@ -90,6 +80,6 @@ grid.arrange(
 # Generate Plots --------------------------
 mergeplot <- ggarrange(pMMR1, pMMR2, ncol=2)
 
-
-
+# Save Output Plots -----------------------
+ggsave(mergeplot, file="StackedArea-GrossNet-Mangroves-00-Myanmar.pdf", width=19.89, height=15, units="cm", dpi=300)
 
