77cd03a515bd8988ca5af311ed6e5570c811d8ce
dondealban <dondealban@gmail.com>
2019-05-14 14:20:13 +0800

    Edited plotting parameters for AYE stacked area plots

--- 3 mmr land cover transitions/scripts/R/MMR.MNG.StackedArea.GrossNetExtentsChanges.Combined.Test.R
+++ 3 mmr land cover transitions/scripts/R/MMR.MNG.StackedArea.GrossNetExtentsChanges.Combined.Test.R
@@ -141,37 +141,35 @@ dfUN2YGN <- data.frame(x=c(2007,2007,2016,2016), y=c(0, unYGN, unYGN, 0)) # undi
 
 # Define Plots ----------------------------
 
-# Left panel
-pMMR1 <- ggplot() + geom_area(aes(x=TimePoint, y=AreaSqKm, fill=factor(Change,
-                                  labels=c("Net Loss",
-                                           "Gross Gain",
-                                           "Persistence"))), 
-                                  data=dfMMR1, alpha=0.6)
-pMMR1 <- pMMR1 + geom_polygon(aes(x=x, y=y, colour="#ff0000"), fill=NA, data=dfGL1)
-pMMR1 <- pMMR1 + geom_polygon(aes(x=x, y=y, colour="#000000"), fill=NA, data=dfUN1)
-pMMR1 <- pMMR1 + scale_colour_manual(values=c("#000000","#ff0000"), labels=c("Undisturbed","Gross Loss"))
-pMMR1 <- pMMR1 + scale_fill_manual(values=c("#ff0000","#00b050","#6666ff"))
-pMMR1 <- pMMR1 + guides(colour=guide_legend(reverse=TRUE))
-pMMR1 <- pMMR1 + labs(x="Interval 1", y="Area (sq.km)")
-pMMR1 <- pMMR1 + scale_x_continuous(breaks=c(1996,2007))
-pMMR1 <- pMMR1 + scale_y_continuous(limits=c(0,12700), breaks=c(0,2500,5000,7500,10000,12500))
-pMMR1 <- pMMR1 + theme_light() + theme(legend.position="none")
+# AYEYARWADY REGION
+pAYE1 <- ggplot() + geom_area(aes(x=TimePoint, y=AreaSqKm, fill=factor(Change,
+                                  labels=c("Net Loss","Gross Gain","Persistence"))), 
+                              data=dfAYE1, alpha=0.6)
+pAYE1 <- pAYE1 + geom_polygon(aes(x=x, y=y, colour="#ff0000"), fill=NA, data=dfGL1AYE)
+pAYE1 <- pAYE1 + geom_polygon(aes(x=x, y=y, colour="#000000"), fill=NA, data=dfUN1AYE)
+pAYE1 <- pAYE1 + scale_colour_manual(values=c("#000000","#ff0000"), labels=c("Undisturbed","Gross Loss"))
+pAYE1 <- pAYE1 + scale_fill_manual(values=c("#ff0000","#00b050","#6666ff"))
+pAYE1 <- pAYE1 + guides(colour=guide_legend(reverse=TRUE))
+pAYE1 <- pAYE1 + labs(x="Interval 1", y="Area (sq.km)")
+pAYE1 <- pAYE1 + scale_x_continuous(breaks=c(1996,2007))
+pAYE1 <- pAYE1 + scale_y_continuous(limits=c(0,5000), breaks=c(0,1000,2000,3000,4000,5000))
+pAYE1 <- pAYE1 + annotate("text", x=1996, y=5000, label="Ayeyarwady", hjust=0, vjust="top", size=5, fontface="bold")
+pAYE1 <- pAYE1 + theme_light() + theme(legend.position="none")
 
-# Right panel
-pMMR2 <- ggplot() + geom_area(aes(x=TimePoint, y=AreaSqKm, fill=factor(Change,
-                                  labels=c("Net Loss",
-                                           "Gross Gain",
-                                           "Persistence"))), 
-                              data=dfMMR2, alpha=0.6)
-pMMR2 <- pMMR2 + geom_polygon(aes(x=x, y=y, colour="#ff0000"), fill=NA, data=dfGL2)
-pMMR2 <- pMMR2 + geom_polygon(aes(x=x, y=y, colour="#000000"), fill=NA, data=dfUN2)
-pMMR2 <- pMMR2 + scale_colour_manual(values=c("#000000","#ff0000"), labels=c("Undisturbed","Gross Loss"))
-pMMR2 <- pMMR2 + scale_fill_manual(values=c("#ff0000","#00b050","#6666ff"))
-pMMR2 <- pMMR2 + guides(colour=guide_legend(reverse=TRUE))
-pMMR2 <- pMMR2 + labs(x="Interval 2", fill=element_blank(), colour="")
-pMMR2 <- pMMR2 + scale_x_continuous(breaks=c(2007,2016))
-pMMR2 <- pMMR2 + scale_y_continuous(limits=c(0,12700), breaks=c(0,2500,5000,7500,10000,12500), position="right")
-pMMR2 <- pMMR2 + theme_light() + theme(axis.title.y=element_blank())
+pAYE2 <- ggplot() + geom_area(aes(x=TimePoint, y=AreaSqKm, fill=factor(Change,
+                                  labels=c("Net Loss","Gross Gain","Persistence"))), 
+                              data=dfAYE2, alpha=0.6)
+pAYE2 <- pAYE2 + geom_polygon(aes(x=x, y=y, colour="#ff0000"), fill=NA, data=dfGL2AYE)
+pAYE2 <- pAYE2 + geom_polygon(aes(x=x, y=y, colour="#000000"), fill=NA, data=dfUN2AYE)
+pAYE2 <- pAYE2 + scale_colour_manual(values=c("#000000","#ff0000"), labels=c("Undisturbed","Gross Loss"))
+pAYE2 <- pAYE2 + scale_fill_manual(values=c("#ff0000","#00b050","#6666ff"))
+pAYE2 <- pAYE2 + guides(colour=guide_legend(reverse=TRUE))
+pAYE2 <- pAYE2 + labs(x="Interval 2")
+pAYE2 <- pAYE2 + scale_x_continuous(breaks=c(2007,2016))
+pAYE2 <- pAYE2 + scale_y_continuous(limits=c(0,5000), breaks=c(0,1000,2000,3000,4000,5000))
+pAYE2 <- pAYE2 + theme_light() + theme(axis.title.y=element_blank(), axis.text.y=element_blank(), legend.position="none")
+
+
 
 # Expose ggplot2 Layouts ------------------
 plotlayout <- lapply(list(pMMR1, pMMR2), expose_layout, FALSE, FALSE)
@@ -182,7 +180,7 @@ grid.arrange(
 )
 
 # Generate Plots --------------------------
-plotMMR <- ggarrange(pMMR1, pMMR2, ncol=2)
+plotAYE <- ggarrange(pAYE1, pAYE2, ncol=2)
 
 # Save Output Plots -----------------------
 ggsave(plotMMR, file="StackedArea-GrossNet-Mangroves-00-Myanmar.pdf", width=19.89, height=15, units="cm", dpi=300)
