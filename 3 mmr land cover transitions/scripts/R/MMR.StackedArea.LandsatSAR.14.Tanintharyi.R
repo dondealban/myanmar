9307e9a504ceab5f6a27e6592ebd7d547e75c42d
dondealban <dondealban@gmail.com>
2019-07-09 16:14:45 +0800

    Updated working dir, land cover classes and codes, plotting parameters

--- 3 mmr land cover transitions/scripts/R/MMR.StackedArea.LandsatSAR.14.Tanintharyi.R
+++ 3 mmr land cover transitions/scripts/R/MMR.StackedArea.LandsatSAR.14.Tanintharyi.R
@@ -12,7 +12,7 @@
 
 
 # Set Working Directory -----------------
-setwd("/Users/dondealban/Dropbox/Research/myanmar/3 mmr land cover transitions/stacked area/14_tanintharyi")
+setwd("/Users/dondealban/Dropbox/Research/myanmar/3 mmr land cover transitions/mangroves/stacked area/5_tanintharyi")
 
 # Load Libraries and Data ---------------
 library(reshape2)
@@ -31,7 +31,7 @@ readdata <- function(filename) {
 
 # Combine as class codes and percentage values in a matrix
 temp <- do.call(rbind, lapply(filenames, readdata))
-colnames(temp) <- c("1","2","3","4","5","6","7","8","9","10")
+colnames(temp) <- c("1","2","3","4","5","6","7","8","9")
 
 # Add years as another column
 row.names(temp) <- c("1996","2007","2016")
@@ -43,7 +43,8 @@ colnames(data) <- c("Years","Class","Percentage")
 # Create Stacked Area Graphs ------------
 
 plot <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
-                   labels=c("Built-up",
+                   labels=c("Bare Ground",
+                            "Built-up",
                             "Forest",
                             "Ice and Snow",
                             "Mangrove",
@@ -51,15 +52,14 @@ plot <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
                             "Rice Paddy",
                             "Rubber Mature",
                             "Shrubland",
-                            "Water",
-                            "Bare Ground"))), 
+                            "Water Body"))), 
                    data=data)
 plot <- plot + labs(title="Net Land Cover Transitions: Tanintharyi Region", x="Year", y="Percentage of Landscape", fill="Land Cover Category")
 plot <- plot + guides(fill=guide_legend(ncol=1))
 plot <- plot + theme_bw()
-plot <- plot + scale_fill_manual(values=c("#ff0000","#246a24","#a6cee3","#6666ff","#ff8000",
-                                          "#a65400","#ff00ff","#ccff66","#66ccff","#ffff66"))
+plot <- plot + scale_fill_manual(values=c("#ffff66","#ff0000","#246a24","#6666ff","#ff8000",
+                                          "#a65400","#ff00ff","#ccff66","#66ccff"))
 plot <- plot + scale_x_continuous(breaks=c(1996,2007,2016))
 plot <- plot + theme(legend.position="none")
 
-ggsave(plot, file="StackedArea-14-Tanintharyi.pdf", width=16, height=15, units="cm", dpi=300)
+ggsave(plot, file="StackedArea-05-Tanintharyi.pdf", width=16, height=15, units="cm", dpi=300)
