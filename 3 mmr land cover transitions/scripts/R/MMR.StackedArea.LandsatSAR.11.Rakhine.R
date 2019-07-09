ac2786cf1cc0ba448c9ea9f1d447f2098437c0af
dondealban <dondealban@gmail.com>
2019-07-09 16:07:49 +0800

    Updated land cover classes, class codes, and plotting parameters

--- 3 mmr land cover transitions/scripts/R/MMR.StackedArea.LandsatSAR.11.Rakhine.R
+++ 3 mmr land cover transitions/scripts/R/MMR.StackedArea.LandsatSAR.11.Rakhine.R
@@ -31,7 +31,7 @@ readdata <- function(filename) {
 
 # Combine as class codes and percentage values in a matrix
 temp <- do.call(rbind, lapply(filenames, readdata))
-colnames(temp) <- c("1","2","4","5","6","7","8","9","10")
+colnames(temp) <- c("1","2","3","4","5","6","7","8","9")
 
 # Add years as another column
 row.names(temp) <- c("1996","2007","2016")
@@ -43,22 +43,22 @@ colnames(data) <- c("Years","Class","Percentage")
 # Create Stacked Area Graphs ------------
 
 plot <- ggplot() + geom_area(aes(x=Years, y=Percentage, fill=factor(Class,
-                   labels=c("Built-up",
+                   labels=c("Bare Ground",
+                            "Built-up",
                             "Forest",
                             "Mangrove",
                             "Oil Palm Mature",
                             "Rice Paddy",
                             "Rubber Mature",
                             "Shrubland",
-                            "Water",
-                            "Bare Ground"))), 
+                            "Water Body"))), 
                    data=data)
 plot <- plot + labs(title="Net Land Cover Transitions: Rakhine State", x="Year", y="Percentage of Landscape", fill="Land Cover Category")
 plot <- plot + guides(fill=guide_legend(ncol=1))
 plot <- plot + theme_bw()
-plot <- plot + scale_fill_manual(values=c("#ff0000","#246a24","#6666ff","#ff8000",
-                                          "#a65400","#ff00ff","#ccff66","#66ccff","#ffff66"))
+plot <- plot + scale_fill_manual(values=c("#ffff66","#ff0000","#246a24","#6666ff","#ff8000",
+                                          "#a65400","#ff00ff","#ccff66","#66ccff"))
 plot <- plot + scale_x_continuous(breaks=c(1996,2007,2016))
 plot <- plot + theme(legend.position="none")
 
-ggsave(plot, file="StackedArea-11-Rakhine.pdf", width=16, height=15, units="cm", dpi=300)
+ggsave(plot, file="StackedArea-04-Rakhine.pdf", width=16, height=15, units="cm", dpi=300)
