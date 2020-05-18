f68712c0c8f973b6bc14ccf35618fa200f88af23
dondealban <dondealban@gmail.com>
2020-05-18 14:46:21 +0800

    Initial commit of new script for plotting WoE results

---
+++ 4 patterns determinants/scripts/weights of evidence/R_TNI_Weights-of-Evidence-Plots.R
@@ -0,0 +1,12 @@
+# This script generates plots from the results of the Weights-of-Evidence (WoE) method
+# implemented in DINAMICA EGO to assess the association of various spatial determinants
+# (e.g., biophysical and socio-economic variables) with the transitions of agricultural
+# expansion of major crop commodities (i.e., oil palm, rubber, rice) in Tanintharyi 
+# Region, Myanmar.
+# 
+# Script by:      Jose Don T. De Alban
+# Date created:   18 May 2020
+# Date modified:  18 May 2020   
+
+
+# ----------------------------------------
