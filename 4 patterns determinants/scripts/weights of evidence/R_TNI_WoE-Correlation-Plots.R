4436fb1cad6c670b794eb589a302eae1ea63ef16
dondealban <dondealban@gmail.com>
2020-05-19 10:01:57 +0800

    Inserted lines to working directories

--- 4 patterns determinants/scripts/weights of evidence/R_TNI_WoE-Correlation-Plots.R
+++ 4 patterns determinants/scripts/weights of evidence/R_TNI_WoE-Correlation-Plots.R
@@ -14,7 +14,10 @@
 
 
 # Set Working Directories ----------------
-
+MainDir  <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/"
+DataDir  <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/"
+DirOPMI1 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/opm/c_1996_2007/step04/"
+DirOPMI2 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/opm/c_2007_2016/step04/"
 
 # Read Data Files ------------------------
 
