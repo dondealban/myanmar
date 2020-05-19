9228f17a7496bb6ae9e9a5fe881cff0920b98833
dondealban <dondealban@gmail.com>
2020-05-19 09:56:17 +0800

    Initial commit of new script

---
+++ 4 patterns determinants/scripts/weights of evidence/R_TNI_WoE-Correlation-Plots.R
@@ -0,0 +1,26 @@
+# This script generates plots from the results of the independence tests implemented
+# in DINAMICA EGO to assess the independence (or association/correlation) of spatial
+# determinants (e.g., biophysical and socio-economic variables) that can be used for
+# calculating the location probabilities of land use/cover transitions related to
+# agriculturalexpansion of major crop commodities (i.e., oil palm, rubber, rice) in
+# Tanintharyi Region, Myanmar.
+# 
+# Script by:      Jose Don T. De Alban
+# Date created:   19 May 2020
+# Date modified:  19 May 2020   
+
+
+# Load Libraries -------------------------
+
+
+# Set Working Directories ----------------
+
+
+# Read Data Files ------------------------
+
+
+# Extract Data Subsets -------------------
+
+
+# Generate Plots -------------------------
+
