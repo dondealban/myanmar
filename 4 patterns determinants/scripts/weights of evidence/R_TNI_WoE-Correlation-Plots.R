df750afcdf3a89ccf664788f1a094197670b16b5
dondealban <dondealban@gmail.com>
2020-05-19 15:22:42 +0800

    Saved temporary lines to test plotting correlation matrix

--- 4 patterns determinants/scripts/weights of evidence/R_TNI_WoE-Correlation-Plots.R
+++ 4 patterns determinants/scripts/weights of evidence/R_TNI_WoE-Correlation-Plots.R
@@ -11,7 +11,10 @@
 
 
 # Load Libraries -------------------------
-
+library(tidyverse)
+#library(GGally)
+library(reshape2)
+#library(ggcorrplot)
 
 # Set Working Directories ----------------
 MainDir  <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/"
@@ -20,10 +23,52 @@ DirOPMI1 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/
 DirOPMI2 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/opm/c_2007_2016/step04/"
 
 # Read Data Files ------------------------
-
+# Correlation data for oil palm
+setwd(DirOPMI1)
+dfOPMi1 <- read.csv(file="tni_correlation.csv", header=TRUE, sep=",")
+setwd(DirOPMI2)
+dfOPMi2 <- read.csv(file="tni_correlation.csv", header=TRUE, sep=",")
 
 # Extract Data Subsets -------------------
 
+# OIL PALM transitions
+
+# Replace character strings
+dfOPMi1$First_Variable.  <- gsub(".*/", "", dfOPMi1$First_Variable.)
+dfOPMi1$Second_Variable. <- gsub(".*/", "", dfOPMi1$Second_Variable.)
+# Round off coefficients
+dfOPMi1$RoundCRM <- round(dfOPMi1$Cramer, digit=2)
+dfOPMi1$RoundJtU <- round(dfOPMi1$Joint_Uncertainty, digit=2)
+
+
+#df3to5i1CRM <- dfOPMi1 %>% filter(dfOPMi1$Transition_From. %in% "3") %>% select(3:4,6)
+
+
+df3to5i1CRM <- dfOPMi1 %>% filter(dfOPMi1$Transition_From. %in% "3" & dfOPMi1$First_Variable. != "distance_to_5") %>% select(3:4,6)
+
+
+# Rename column names
+# colnames(df3to5i1CRM) <- c("x","y","coefficient","label")
+
+#Not this
+df2 <- spread(df3to5i1CRM, Second_Variable., Cramer)
+df2[is.na(df2)] <- 0
+ggcorrplot(df2, method="square")
+
+#This!
+df3 <- acast(df3to5i1CRM, First_Variable. ~ Second_Variable., value.var="Cramer")
+df3[is.na(df3)] <- 0
+ggcorrplot(df3, method="square")
+
+
+cMTCARS <- cor(mtcars)
+ggcorrplot(cMTCARS)
 
 # Generate Plots -------------------------
+
+plot <- ggcorr(df3to5i1CRM, geom="blank", label=TRUE, hjust=0.75)
+plot <- plot + geom_point(size=10, aes(color=coefficient > 0.45, alpha = abs(coefficient) > 0.5))
+plot <- plot + scale_alpha_manual(values=c("TRUE"=0.25, "FALSE"=0))
+plot <- plot + guides(color=FALSE, alpha=FALSE)
+
 
