cfdef3a84b726979e249075a4ba759285e8ebc27
dondealban <dondealban@gmail.com>
2020-06-16 15:56:28 +0800

    Inserted lines for loading required libraries, setting working directories

--- 4 patterns determinants/scripts/hotspot analysis/R_TNI_Plots-Hotspot-Analysis.R
+++ 4 patterns determinants/scripts/hotspot analysis/R_TNI_Plots-Hotspot-Analysis.R
@@ -1,4 +1,4 @@
-# This script generates plots from the results of the Getis-Ord Gi statistic or Hotspots
+# This script generates plots from the results of the Getis-Ord Gi statistic or Hotspot
 # analysis implemented in ArcGIS (Emerging Hotspots Toolbox) to assess the spatial 
 # associations and distribution of the different gain transitions of major agricultural
 # crop commodities (i.e., oil palm, rubber, rice) in Tanintharyi Region, Myanmar.
@@ -7,3 +7,13 @@
 # Date created:   16 Jun 2020
 # Date modified:  
 
+
+# Load Libraries -------------------------
+library(tidyverse)
+
+# Set Working Directories ----------------
+DirMAIN <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/"
+DirDATA <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/hotspot analysis/"
+DirOPM  <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/hotspot analysis/csv opm/"
+DirRBR  <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/hotspot analysis/csv rbr/"
+DirRPD  <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/hotspot analysis/csv rpd/"
