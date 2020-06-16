760efa13b1c5a63a9fafd66c49d16d3d025085d2
dondealban <dondealban@gmail.com>
2020-06-16 15:53:12 +0800

    Initial commit of new script

---
+++ 4 patterns determinants/scripts/hotspot analysis/R_TNI_Plots-Hotspot-Analysis.R
@@ -0,0 +1,9 @@
+# This script generates plots from the results of the Getis-Ord Gi statistic or Hotspots
+# analysis implemented in ArcGIS (Emerging Hotspots Toolbox) to assess the spatial 
+# associations and distribution of the different gain transitions of major agricultural
+# crop commodities (i.e., oil palm, rubber, rice) in Tanintharyi Region, Myanmar.
+# 
+# Script by:      Jose Don T. De Alban
+# Date created:   16 Jun 2020
+# Date modified:  
+
