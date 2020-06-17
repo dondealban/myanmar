7b970d12b14ce27d03c0fcf1cc7e619450146d80
dondealban <dondealban@gmail.com>
2020-06-17 11:25:55 +0800

    Revised variable names

--- 4 patterns determinants/scripts/weights of evidence/run 2/R_TNI_Run2_WoE-Contrast-Plots_RPD-All-Transitions.R
+++ 4 patterns determinants/scripts/weights of evidence/run 2/R_TNI_Run2_WoE-Contrast-Plots_RPD-All-Transitions.R
@@ -15,64 +15,64 @@ library(tidyverse)
 # Set Working Directories ----------------
 DirMAIN  <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/"
 DirDATA  <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/run 2/plots/"
-DirRBRI1 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/run 2/rbr/c_1996_2007/step03/"
-DirRBRI2 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/run 2/rbr/c_2007_2016/step03/"
+DirRPDI1 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/run 2/rpd/c_1996_2007/step03/"
+DirRPDI2 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/weights of evidence/run 2/rpd/c_2007_2016/step03/"
 
 # Read Data Files ------------------------
-# WoE data for rubber
-setwd(DirRBRI1)
-csvRBRi1 <- read.csv(file="tni_weights.csv", header=TRUE, sep=",")
-setwd(DirRBRI2)
-csvRBRi2 <- read.csv(file="tni_weights.csv", header=TRUE, sep=",")
+# WoE data for rice paddy
+setwd(DirRPDI1)
+csvRPDi1 <- read.csv(file="tni_weights.csv", header=TRUE, sep=",")
+setwd(DirRPDI2)
+csvRPDi2 <- read.csv(file="tni_weights.csv", header=TRUE, sep=",")
 
 # Extract Data Subsets -------------------
 
 # Replace character strings
-csvRBRi1$Variable. <- gsub(".*/", "", csvRBRi1$Variable.)
-csvRBRi2$Variable. <- gsub(".*/", "", csvRBRi2$Variable.)
+csvRPDi1$Variable. <- gsub(".*/", "", csvRPDi1$Variable.)
+csvRPDi2$Variable. <- gsub(".*/", "", csvRPDi2$Variable.)
 # Remove rows in dataframes that satisfy conditions
-dfRBRi1 <- csvRBRi1 %>% filter(!(Significant == 0))
-dfRBRi2 <- csvRBRi2 %>% filter(!(Significant == 0))
+dfRPDi1 <- csvRPDi1 %>% filter(!(Significant == 0))
+dfRPDi2 <- csvRPDi2 %>% filter(!(Significant == 0))
 
 # Generate Plots -------------------------
 
 # Plot with transitions from all source land cover types
 # WoE Contrast values: 1996-2007
-pRBRi1con <- ggplot() + geom_line(data=dfRBRi1, aes(x=Range_Upper_Limit., y=Contrast, colour=as.factor(Transition_From.)))
-pRBRi1con <- pRBRi1con + facet_wrap(~ Variable., scales="free")
-pRBRi1con <- pRBRi1con + labs(title="Association of Spatial Determinants with Rubber Gain Transitions",
+pRPDi1con <- ggplot() + geom_line(data=dfRPDi1, aes(x=Range_Upper_Limit., y=Contrast, colour=as.factor(Transition_From.)))
+pRPDi1con <- pRPDi1con + facet_wrap(~ Variable., scales="free")
+pRPDi1con <- pRPDi1con + labs(title="Association of Spatial Determinants with Rubber Gain Transitions",
                               subtitle="Time-Interval: 1996-2007", x="Ranges", y="Contrast")
-pRBRi1con <- pRBRi1con + scale_colour_manual(name="Source Land Cover",
+pRPDi1con <- pRPDi1con + scale_colour_manual(name="Source Land Cover",
                                              values=c("#246a24","#6666ff","#ff8000","#a65400","#ccff66"),
                                              labels=c("Forest","Mangrove","Oil Palm","Rice Paddy","Shrub/Orchard"))
 # WoE Contrast values: 2007-2016
-pRBRi2con <- ggplot() + geom_line(data=dfRBRi2, aes(x=Range_Upper_Limit., y=Contrast, colour=as.factor(Transition_From.)))
-pRBRi2con <- pRBRi2con + facet_wrap(~ Variable., scales="free")
-pRBRi2con <- pRBRi2con + labs(title="Association of Spatial Determinants with Rubber Gain Transitions",
+pRPDi2con <- ggplot() + geom_line(data=dfRPDi2, aes(x=Range_Upper_Limit., y=Contrast, colour=as.factor(Transition_From.)))
+pRPDi2con <- pRPDi2con + facet_wrap(~ Variable., scales="free")
+pRPDi2con <- pRPDi2con + labs(title="Association of Spatial Determinants with Rubber Gain Transitions",
                               subtitle="Time-Interval: 2007-2016", x="Ranges", y="Contrast")
-pRBRi2con <- pRBRi2con + scale_colour_manual(name="Source Land Cover",
+pRPDi2con <- pRPDi2con + scale_colour_manual(name="Source Land Cover",
                                              values=c("#246a24","#6666ff","#ff8000","#a65400","#ccff66"),
                                              labels=c("Forest","Mangrove","Oil Palm","Rice Paddy","Shrub/Orchard"))
 # WoE Coefficients: 1996-2007
-pRBRi1woe <- ggplot() + geom_line(data=dfRBRi1, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=as.factor(Transition_From.)))
-pRBRi1woe <- pRBRi1woe + facet_wrap(~ Variable., scales="free")
-pRBRi1woe <- pRBRi1woe + labs(title="Association of Spatial Determinants with Rubber Gain Transitions",
+pRPDi1woe <- ggplot() + geom_line(data=dfRPDi1, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=as.factor(Transition_From.)))
+pRPDi1woe <- pRPDi1woe + facet_wrap(~ Variable., scales="free")
+pRPDi1woe <- pRPDi1woe + labs(title="Association of Spatial Determinants with Rubber Gain Transitions",
                               subtitle="Time-Interval: 1996-2007", x="Ranges", y="Weights-of-Evidence Coefficients")
-pRBRi1woe <- pRBRi1woe + scale_colour_manual(name="Source Land Cover",
+pRPDi1woe <- pRPDi1woe + scale_colour_manual(name="Source Land Cover",
                                              values=c("#246a24","#6666ff","#ff8000","#a65400","#ccff66"),
                                              labels=c("Forest","Mangrove","Oil Palm","Rice Paddy","Shrub/Orchard"))
 # WoE Coefficients: 2007-2016
-pRBRi2woe <- ggplot() + geom_line(data=dfRBRi2, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=as.factor(Transition_From.)))
-pRBRi2woe <- pRBRi2woe + facet_wrap(~ Variable., scales="free")
-pRBRi2woe <- pRBRi2woe + labs(title="Association of Spatial Determinants with Rubber Gain Transitions",
+pRPDi2woe <- ggplot() + geom_line(data=dfRPDi2, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=as.factor(Transition_From.)))
+pRPDi2woe <- pRPDi2woe + facet_wrap(~ Variable., scales="free")
+pRPDi2woe <- pRPDi2woe + labs(title="Association of Spatial Determinants with Rubber Gain Transitions",
                               subtitle="Time-Interval: 2007-2016", x="Ranges", y="Weights-of-Evidence Coefficients")
-pRBRi2woe <- pRBRi2woe + scale_colour_manual(name="Source Land Cover",
+pRPDi2woe <- pRPDi2woe + scale_colour_manual(name="Source Land Cover",
                                              values=c("#246a24","#6666ff","#ff8000","#a65400","#ccff66"),
                                              labels=c("Forest","Mangrove","Oil Palm","Rice Paddy","Shrub/Orchard"))
 
 # Save Output Plots ----------------------
 setwd(DirDATA)
-ggsave(pRBRi1con, file="TNI_AllTransitions_SD_RBR_I1_Con.pdf", width=40, height=30, units="cm", dpi=300)
-ggsave(pRBRi2con, file="TNI_AllTransitions_SD_RBR_I2_Con.pdf", width=40, height=30, units="cm", dpi=300)
-ggsave(pRBRi1woe, file="TNI_AllTransitions_SD_RBR_I1_WoE.pdf", width=40, height=30, units="cm", dpi=300)
-ggsave(pRBRi2woe, file="TNI_AllTransitions_SD_RBR_I2_WoE.pdf", width=40, height=30, units="cm", dpi=300)
+ggsave(pRPDi1con, file="TNI_AllTransitions_SD_RPD_I1_Con.pdf", width=40, height=30, units="cm", dpi=300)
+ggsave(pRPDi2con, file="TNI_AllTransitions_SD_RPD_I2_Con.pdf", width=40, height=30, units="cm", dpi=300)
+ggsave(pRPDi1woe, file="TNI_AllTransitions_SD_RPD_I1_WoE.pdf", width=40, height=30, units="cm", dpi=300)
+ggsave(pRPDi2woe, file="TNI_AllTransitions_SD_RPD_I2_WoE.pdf", width=40, height=30, units="cm", dpi=300)
