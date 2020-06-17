ccc3802fdaf64709fa75be376877099442a472dc
dondealban <dondealban@gmail.com>
2020-06-17 21:57:11 +0800

    Added lines for constructing dataframes and plots for relative variable importance

--- 4 patterns determinants/scripts/weights of evidence/run 2/R_TNI_Run2_WoE-Contrast-Plots_RBR-All-Transitions.R
+++ 4 patterns determinants/scripts/weights of evidence/run 2/R_TNI_Run2_WoE-Contrast-Plots_RBR-All-Transitions.R
@@ -34,6 +34,18 @@ csvRBRi2$Variable. <- gsub(".*/", "", csvRBRi2$Variable.)
 dfRBRi1 <- csvRBRi1 %>% filter(!(Significant == 0))
 dfRBRi2 <- csvRBRi2 %>% filter(!(Significant == 0))
 
+# Construct dataframes for relative importance plots
+impRBRi1 <- dfRBRi1 %>% filter(Contrast >= 0)
+impRBRi2 <- dfRBRi2 %>% filter(Contrast >= 0)
+# Create new column with concatenated data from three columns
+impRBRi1$VariableLimits <- trimws(paste(impRBRi1$Variable.,"_",
+                                        impRBRi1$Range_Lower_Limit.,"_",impRBRi1$Range_Upper_Limit.), which=c("both"))
+impRBRi2$VariableLimits <- trimws(paste(impRBRi2$Variable.,"_",
+                                        impRBRi2$Range_Lower_Limit.,"_",impRBRi2$Range_Upper_Limit.), which=c("both"))
+# Select top 5 variables per gain transition
+imp5RBRi1 <- impRBRi1 %>% arrange(desc(Contrast)) %>% group_by(Transition_From.) %>% slice(1:5)
+imp5RBRi2 <- impRBRi2 %>% arrange(desc(Contrast)) %>% group_by(Transition_From.) %>% slice(1:5)
+
 # Generate Plots -------------------------
 
 # Plot with transitions from all source land cover types
@@ -70,9 +82,25 @@ pRBRi2woe <- pRBRi2woe + scale_colour_manual(name="Source Land Cover",
                                              values=c("#246a24","#6666ff","#ff8000","#a65400","#ccff66"),
                                              labels=c("Forest","Mangrove","Oil Palm","Rice Paddy","Shrub/Orchard"))
 
+# Relative importance of spatial determinants based on WoE Contrast
+# WoE Contrast values: 1996-2007
+pRBRi1imp <- ggplot() + geom_col(data=imp5RBRi1, aes(x=VariableLimits, y=Contrast), position="dodge")
+pRBRi1imp <- pRBRi1imp + coord_flip()
+pRBRi1imp <- pRBRi1imp + facet_grid(rows=vars(Transition_From.), scales="free")
+pRBRi1imp <- pRBRi1imp + labs(title="Top 5 Spatial Determinants for Rubber Gain Transitions",
+                              subtitle="Time-Interval: 1996-2007", x="Variables [Lower-Upper Range Limits]", y="Contrast")
+# WoE Contrast values: 2007-2016
+pRBRi2imp <- ggplot() + geom_col(data=imp5RBRi2, aes(x=VariableLimits, y=Contrast), position="dodge")
+pRBRi2imp <- pRBRi2imp + coord_flip()
+pRBRi2imp <- pRBRi2imp + facet_grid(rows=vars(Transition_From.), scales="free")
+pRBRi2imp <- pRBRi2imp + labs(title="Top 5 Spatial Determinants for Rubber Gain Transitions",
+                              subtitle="Time-Interval: 2007-2016", x="Variables [Lower-Upper Range Limits]", y="Contrast")
+
 # Save Output Plots ----------------------
 setwd(DirDATA)
 ggsave(pRBRi1con, file="TNI_AllTransitions_SD_RBR_I1_Con.pdf", width=40, height=30, units="cm", dpi=300)
 ggsave(pRBRi2con, file="TNI_AllTransitions_SD_RBR_I2_Con.pdf", width=40, height=30, units="cm", dpi=300)
 ggsave(pRBRi1woe, file="TNI_AllTransitions_SD_RBR_I1_WoE.pdf", width=40, height=30, units="cm", dpi=300)
 ggsave(pRBRi2woe, file="TNI_AllTransitions_SD_RBR_I2_WoE.pdf", width=40, height=30, units="cm", dpi=300)
+ggsave(pRBRi1imp, file="TNI_AllTransitions_SD_RBR_I1_Imp.pdf", width=20, height=25, units="cm", dpi=300)
+ggsave(pRBRi2imp, file="TNI_AllTransitions_SD_RBR_I2_Imp.pdf", width=20, height=25, units="cm", dpi=300)
