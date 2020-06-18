8196268f5b89a5e488c9951560dc1d423d1ff8b1
dondealban <dondealban@gmail.com>
2020-06-18 16:57:59 +0800

    Edited WoE per OPM gain transition script

--- 4 patterns determinants/scripts/weights of evidence/run 2/R_TNI_Run2_WoE-Contrast-Plots_OPM-Per-Transition.R
+++ 4 patterns determinants/scripts/weights of evidence/run 2/R_TNI_Run2_WoE-Contrast-Plots_OPM-Per-Transition.R
@@ -21,129 +21,166 @@ DirOPMI2 <- "/Users/dondealban/Dropbox/Research/myanmar/4 patterns determinants/
 # Read Data Files ------------------------
 # WoE data for oil palm
 setwd(DirOPMI1)
-dfOPMi1 <- read.csv(file="tni_weights.csv", header=TRUE, sep=",")
+csvOPMi1 <- read.csv(file="tni_weights.csv", header=TRUE, sep=",")
 setwd(DirOPMI2)
-dfOPMi2 <- read.csv(file="tni_weights.csv", header=TRUE, sep=",")
+csvOPMi2 <- read.csv(file="tni_weights.csv", header=TRUE, sep=",")
 
-# Clean Data Files -----------------------
+# Construct Omnibus Dataframe ------------
 # Replace character strings
-dfOPMi1$Variable. <- gsub(".*/", "", dfOPMi1$Variable.)
-dfOPMi2$Variable. <- gsub(".*/", "", dfOPMi2$Variable.)
+csvOPMi1$Variable. <- gsub(".*/", "", csvOPMi1$Variable.)
+csvOPMi2$Variable. <- gsub(".*/", "", csvOPMi2$Variable.)
+# Remove rows in dataframes that satisfy conditions
+dfOPMi1 <- csvOPMi1 %>% filter(!(Significant == 0))
+dfOPMi2 <- csvOPMi2 %>% filter(!(Significant == 0 | Variable. == "D_DefoI1"))
 # Add columns to indicate time-intervals
 dfOPMi1$Time.Interval <- rep("1996-2007", nrow(dfOPMi1))
 dfOPMi2$Time.Interval <- rep("2007-2016", nrow(dfOPMi2))
 # Combine dataframes
-dfOPMmerge <- rbind(dfOPMi1, dfOPMi2)
+dfOPMall <- rbind(dfOPMi1, dfOPMi2)
 
-# Extract Data Subsets -------------------
-# Extract relevant spatial determinants based on literature
-dfOPM <- dfOPMmerge %>% filter(Variable. %in% "Elev" | Variable. %in% "Slope" |
-                               Variable. %in% "Acc15" | Variable. %in% "PDn15" |
-                               Variable. %in% "Bio01" | Variable. %in% "Bio06" | Variable. %in% "Bio12" |
-                               Variable. %in% "Bio13" | Variable. %in% "Bio14" |
-                               Variable. %in% "distance_to_5" | Variable. %in% "D_River" | Variable. %in% "D_Road" |
-                               Variable. %in% "D_Town" | Variable. %in% "D_Vill" |
-                               Variable. %in% "Cy000" | Variable. %in% "Sd000" | Variable. %in% "BD000" | 
-                               Variable. %in% "OC000" | Variable. %in% "PH000" | Variable. %in% "WC000")
-# Excluded spatial determinants
-#                               Variable. %in% "Bio02" | Variable. %in% "Bio03" | Variable. %in% "Bio04" |
-#                               Variable. %in% "Bio05" | Variable. %in% "Bio07" | Variable. %in% "Bio08" |
-#                               Variable. %in% "Bio09" | Variable. %in% "Bio10" | Variable. %in% "Bio11" |
-#                               Variable. %in% "Bio15" | Variable. %in% "Bio16" | Variable. %in% "Bio17" |
-#                               Variable. %in% "Bio18" | Variable. %in% "Bio19" |
-#                               Variable. %in% "S_Txn" | Variable. %in% "Tx000" |
-#                               Variable. %in% "D_Opm1996" | Variable. %in% "D_Opm2007" | Variable. %in% "D_Opm2016" |
-#                               Variable. %in% "D_Mill" | Variable. %in% "Acc00" | Variable. %in% "PDn05" |
+# Extract subsets per transition
+dfFORtoOPM <- dfOPMall %>% filter(dfOPMall$Transition_From. %in% "3")
+dfMNGtoOPM <- dfOPMall %>% filter(dfOPMall$Transition_From. %in% "4")
+dfRPDtoOPM <- dfOPMall %>% filter(dfOPMall$Transition_From. %in% "6")
+dfRBRtoOPM <- dfOPMall %>% filter(dfOPMall$Transition_From. %in% "7")
+dfSHBtoOPM <- dfOPMall %>% filter(dfOPMall$Transition_From. %in% "8")
 
+# Generate Plots: All Variables Per Transition ----------
 
-# Extract per transition
-dfFORtoOPM <- dfOPM %>% filter(dfOPM$Transition_From. %in% "3")
-dfMNGtoOPM <- dfOPM %>% filter(dfOPM$Transition_From. %in% "4")
-dfRPDtoOPM <- dfOPM %>% filter(dfOPM$Transition_From. %in% "6")
-dfRBRtoOPM <- dfOPM %>% filter(dfOPM$Transition_From. %in% "7")
-dfSHBtoOPM <- dfOPM %>% filter(dfOPM$Transition_From. %in% "8")
+# WoE Contrast
 
-# Generate Per Transition Plots ----------
-
-# FOR to OPM
-
-# WoE contrast values
-pFORtoOPMcon <- ggplot() + geom_line(data=dfFORtoOPM, aes(x=Range_Lower_Limit., y=Contrast, colour=Time.Interval))
+# FOR to OPM: WoE Contrast values
+pFORtoOPMcon <- ggplot() + geom_line(data=dfFORtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
 pFORtoOPMcon <- pFORtoOPMcon + facet_wrap(~ Variable., scales="free_x")
 pFORtoOPMcon <- pFORtoOPMcon + labs(title="Association of Spatial Determinants with FOR to OPM Transition",
                                     x="Ranges", y="Contrast", colour="Time-Interval")
-# WoE coefficients
-pFORtoOPMwoe <- ggplot() + geom_line(data=dfFORtoOPM, aes(x=Range_Lower_Limit., y=Weight_Coefficient, colour=Time.Interval))
-pFORtoOPMwoe <- pFORtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
-pFORtoOPMwoe <- pFORtoOPMwoe + labs(title="Association of Spatial Determinants with FOR to OPM Transition",
-                                    x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
-
-# MNG to OPM
-
-# WoE contrast values
-pMNGtoOPMcon <- ggplot() + geom_line(data=dfMNGtoOPM, aes(x=Range_Lower_Limit., y=Contrast, colour=Time.Interval))
+# MNG to OPM: WoE Contrast values
+pMNGtoOPMcon <- ggplot() + geom_line(data=dfMNGtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
 pMNGtoOPMcon <- pMNGtoOPMcon + facet_wrap(~ Variable., scales="free_x")
 pMNGtoOPMcon <- pMNGtoOPMcon + labs(title="Association of Spatial Determinants with MNG to OPM Transition",
                                     x="Ranges", y="Contrast", colour="Time-Interval")
-# WoE coefficients
-pMNGtoOPMwoe <- ggplot() + geom_line(data=dfMNGtoOPM, aes(x=Range_Lower_Limit., y=Weight_Coefficient, colour=Time.Interval))
-pMNGtoOPMwoe <- pMNGtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
-pMNGtoOPMwoe <- pMNGtoOPMwoe + labs(title="Association of Spatial Determinants with MNG to OPM Transition",
-                                    x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
-
-# RPD to OPM
-
-# WoE contrast values
-pRPDtoOPMcon <- ggplot() + geom_line(data=dfRPDtoOPM, aes(x=Range_Lower_Limit., y=Contrast, colour=Time.Interval))
+# RPD to OPM: WoE Contrast values
+pRPDtoOPMcon <- ggplot() + geom_line(data=dfRPDtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
 pRPDtoOPMcon <- pRPDtoOPMcon + facet_wrap(~ Variable., scales="free_x")
 pRPDtoOPMcon <- pRPDtoOPMcon + labs(title="Association of Spatial Determinants with RPD to OPM Transition",
                                     x="Ranges", y="Contrast", colour="Time-Interval")
-# WoE coefficients
-pRPDtoOPMwoe <- ggplot() + geom_line(data=dfRPDtoOPM, aes(x=Range_Lower_Limit., y=Weight_Coefficient, colour=Time.Interval))
-pRPDtoOPMwoe <- pRPDtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
-pRPDtoOPMwoe <- pRPDtoOPMwoe + labs(title="Association of Spatial Determinants with RPD to OPM Transition",
-                                    x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
-
-# RBR to OPM
-
-# WoE contrast values
-pRBRtoOPMcon <- ggplot() + geom_line(data=dfRBRtoOPM, aes(x=Range_Lower_Limit., y=Contrast, colour=Time.Interval))
+# RBR to OPM: WoE Contrast values
+pRBRtoOPMcon <- ggplot() + geom_line(data=dfRBRtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
 pRBRtoOPMcon <- pRBRtoOPMcon + facet_wrap(~ Variable., scales="free_x")
 pRBRtoOPMcon <- pRBRtoOPMcon + labs(title="Association of Spatial Determinants with RBR to OPM Transition",
                                     x="Ranges", y="Contrast", colour="Time-Interval")
-# WoE coefficients
-pRBRtoOPMwoe <- ggplot() + geom_line(data=dfRBRtoOPM, aes(x=Range_Lower_Limit., y=Weight_Coefficient, colour=Time.Interval))
-pRBRtoOPMwoe <- pRBRtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
-pRBRtoOPMwoe <- pRBRtoOPMwoe + labs(title="Association of Spatial Determinants with RBR to OPM Transition",
-                                    x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
-
-# SHB to OPM
-
-# WoE contrast values
-pSHBtoOPMcon <- ggplot() + geom_line(data=dfSHBtoOPM, aes(x=Range_Lower_Limit., y=Contrast, colour=Time.Interval))
+# SHB to OPM: WoE Contrast values
+pSHBtoOPMcon <- ggplot() + geom_line(data=dfSHBtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
 pSHBtoOPMcon <- pSHBtoOPMcon + facet_wrap(~ Variable., scales="free_x")
 pSHBtoOPMcon <- pSHBtoOPMcon + labs(title="Association of Spatial Determinants with SHB to OPM Transition",
                                     x="Ranges", y="Contrast", colour="Time-Interval")
-# WoE coefficients
-pSHBtoOPMwoe <- ggplot() + geom_line(data=dfSHBtoOPM, aes(x=Range_Lower_Limit., y=Weight_Coefficient, colour=Time.Interval))
+
+# WoE Coefficients
+
+# FOR to OPM: WoE coefficients
+pFORtoOPMwoe <- ggplot() + geom_line(data=dfFORtoOPM, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=Time.Interval))
+pFORtoOPMwoe <- pFORtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
+pFORtoOPMwoe <- pFORtoOPMwoe + labs(title="Association of Spatial Determinants with FOR to OPM Transition",
+                                    x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
+# MNG to OPM: WoE coefficients
+pMNGtoOPMwoe <- ggplot() + geom_line(data=dfMNGtoOPM, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=Time.Interval))
+pMNGtoOPMwoe <- pMNGtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
+pMNGtoOPMwoe <- pMNGtoOPMwoe + labs(title="Association of Spatial Determinants with MNG to OPM Transition",
+                                    x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
+# RPD to OPM: WoE coefficients
+pRPDtoOPMwoe <- ggplot() + geom_line(data=dfRPDtoOPM, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=Time.Interval))
+pRPDtoOPMwoe <- pRPDtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
+pRPDtoOPMwoe <- pRPDtoOPMwoe + labs(title="Association of Spatial Determinants with RPD to OPM Transition",
+                                    x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
+# RBR to OPM: WoE coefficients
+pRBRtoOPMwoe <- ggplot() + geom_line(data=dfRBRtoOPM, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=Time.Interval))
+pRBRtoOPMwoe <- pRBRtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
+pRBRtoOPMwoe <- pRBRtoOPMwoe + labs(title="Association of Spatial Determinants with RBR to OPM Transition",
+                                    x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
+# SHB to OPM: WoE coefficients
+pSHBtoOPMwoe <- ggplot() + geom_line(data=dfSHBtoOPM, aes(x=Range_Upper_Limit., y=Weight_Coefficient, colour=Time.Interval))
 pSHBtoOPMwoe <- pSHBtoOPMwoe + facet_wrap(~ Variable., scales="free_x")
 pSHBtoOPMwoe <- pSHBtoOPMwoe + labs(title="Association of Spatial Determinants with SHB to OPM Transition",
                                     x="Ranges", y="Weights-of-Evidence Coefficients", colour="Time-Interval")
 
+# Save Plots: All Variables Per Transition --------------
+setwd(DirPLOT)
+ggsave(pFORtoOPMcon, file="TNI_AllVariables_FORtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pMNGtoOPMcon, file="TNI_AllVariables_MNGtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pRPDtoOPMcon, file="TNI_AllVariables_RPDtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pRBRtoOPMcon, file="TNI_AllVariables_RBRtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pSHBtoOPMcon, file="TNI_AllVariables_SHBtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pFORtoOPMwoe, file="TNI_AllVariables_FORtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pMNGtoOPMwoe, file="TNI_AllVariables_MNGtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pRPDtoOPMwoe, file="TNI_AllVariables_RPDtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pRBRtoOPMwoe, file="TNI_AllVariables_RBRtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pSHBtoOPMwoe, file="TNI_AllVariables_SHBtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
 
-# Save Output Plots ----------------------
-setwd(DirDATA)
-ggsave(pFORtoOPMcon, file="TNI_FORtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
-ggsave(pFORtoOPMwoe, file="TNI_FORtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
 
-ggsave(pMNGtoOPMcon, file="TNI_MNGtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
-ggsave(pMNGtoOPMwoe, file="TNI_MNGtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
+# Extract Data Subsets -------------------
+# Extract relevant spatial determinants based on literature
+subOPM <- dfOPMall %>% filter(Variable. %in% c("Bio01","Bio06","Bio12","D_Mill","distance_to_5","D_Road","D_Town","D_Vill",
+                                               "Elev","PDn05","PDn15","Slope","BD000","Cy000","OC000","PH000","Sd000",
+                                               "Tx000","WC000"))
+# Alternates
+# subOPM <- dfOPMall %>% filter(!(Variable. %in% c("D_Opm1996","D_Opm2007","D_ComF","D_KBA","D_OPC","D_PA","D_ResF")))
 
-ggsave(pRPDtoOPMcon, file="TNI_RPDtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
-ggsave(pRPDtoOPMwoe, file="TNI_RPDtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
+# Excluded spatial determinants
+# Variable. %in% "Elev" | Variable. %in% "Slope" | Variable. %in% "Acc00" | Variable. %in% "Acc15" | 
+# Variable. %in% "Bio01" | Variable. %in% "Bio02" | Variable. %in% "Bio03" | Variable. %in% "Bio04" |
+# Variable. %in% "Bio05" | Variable. %in% "Bio06" | Variable. %in% "Bio07" | Variable. %in% "Bio08" |
+# Variable. %in% "Bio09" | Variable. %in% "Bio10" | Variable. %in% "Bio11" | Variable. %in% "Bio12" |
+# Variable. %in% "Bio13" | Variable. %in% "Bio14" | Variable. %in% "Bio15" | Variable. %in% "Bio16" | 
+# Variable. %in% "Bio17" | Variable. %in% "Bio18" | Variable. %in% "Bio19" |
+# Variable. %in% "distance_to_5" | Variable. %in% "D_Opm1996" | Variable. %in% "D_Opm2007" | 
+# Variable. %in% "D_Mill" | Variable. %in% "D_River" | Variable. %in% "D_Road" |
+# Variable. %in% "D_Town" | Variable. %in% "D_Vill" |
+# Variable. %in% "PDn05" | Variable. %in% "PDn15" |
+# Variable. %in% "Tx000" | Variable. %in% "Cy000" | Variable. %in% "Sd000" | Variable. %in% "BD000" | 
+# Variable. %in% "OC000" | Variable. %in% "PH000" | Variable. %in% "WC000")
 
-ggsave(pRBRtoOPMcon, file="TNI_RBRtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
-ggsave(pRBRtoOPMwoe, file="TNI_RBRtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
+# Extract per transition
+subFORtoOPM <- subOPM %>% filter(subOPM$Transition_From. %in% "3")
+subMNGtoOPM <- subOPM %>% filter(subOPM$Transition_From. %in% "4")
+subRPDtoOPM <- subOPM %>% filter(subOPM$Transition_From. %in% "6")
+subRBRtoOPM <- subOPM %>% filter(subOPM$Transition_From. %in% "7")
+subSHBtoOPM <- subOPM %>% filter(subOPM$Transition_From. %in% "8")
 
-ggsave(pSHBtoOPMcon, file="TNI_SHBtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
-ggsave(pSHBtoOPMwoe, file="TNI_SHBtoOPM_SD_WoE.pdf", width=30, height=20, units="cm", dpi=300)
+# Generate Plots: Select Variables Per Transition -------
+
+# WoE Contrast
+
+# FOR to OPM: WoE Contrast values
+pFORtoOPMcon_sub <- ggplot() + geom_line(data=subFORtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
+pFORtoOPMcon_sub <- pFORtoOPMcon_sub + facet_wrap(~ Variable., scales="free_x")
+pFORtoOPMcon_sub <- pFORtoOPMcon_sub + labs(title="Association of Spatial Determinants with FOR to OPM Transition",
+                                    x="Ranges", y="Contrast", colour="Time-Interval")
+# MNG to OPM: WoE Contrast values
+pMNGtoOPMcon_sub <- ggplot() + geom_line(data=subMNGtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
+pMNGtoOPMcon_sub <- pMNGtoOPMcon_sub + facet_wrap(~ Variable., scales="free_x")
+pMNGtoOPMcon_sub <- pMNGtoOPMcon_sub + labs(title="Association of Spatial Determinants with MNG to OPM Transition",
+                                    x="Ranges", y="Contrast", colour="Time-Interval")
+# RPD to OPM: WoE Contrast values
+pRPDtoOPMcon_sub <- ggplot() + geom_line(data=subRPDtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
+pRPDtoOPMcon_sub <- pRPDtoOPMcon_sub + facet_wrap(~ Variable., scales="free_x")
+pRPDtoOPMcon_sub <- pRPDtoOPMcon_sub + labs(title="Association of Spatial Determinants with RPD to OPM Transition",
+                                    x="Ranges", y="Contrast", colour="Time-Interval")
+# RBR to OPM: WoE Contrast values
+pRBRtoOPMcon_sub <- ggplot() + geom_line(data=subRBRtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
+pRBRtoOPMcon_sub <- pRBRtoOPMcon_sub + facet_wrap(~ Variable., scales="free_x")
+pRBRtoOPMcon_sub <- pRBRtoOPMcon_sub + labs(title="Association of Spatial Determinants with RBR to OPM Transition",
+                                    x="Ranges", y="Contrast", colour="Time-Interval")
+# SHB to OPM: WoE Contrast values
+pSHBtoOPMcon_sub <- ggplot() + geom_line(data=subSHBtoOPM, aes(x=Range_Upper_Limit., y=Contrast, colour=Time.Interval))
+pSHBtoOPMcon_sub <- pSHBtoOPMcon_sub + facet_wrap(~ Variable., scales="free_x")
+pSHBtoOPMcon_sub <- pSHBtoOPMcon_sub + labs(title="Association of Spatial Determinants with SHB to OPM Transition",
+                                    x="Ranges", y="Contrast", colour="Time-Interval")
+
+# Save Plots: Select Variables Per Transition -----------
+setwd(DirPLOT)
+ggsave(pFORtoOPMcon_sub, file="TNI_SelectVariables_FORtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pMNGtoOPMcon_sub, file="TNI_SelectVariables_MNGtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pRPDtoOPMcon_sub, file="TNI_SelectVariables_RPDtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pRBRtoOPMcon_sub, file="TNI_SelectVariables_RBRtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+ggsave(pSHBtoOPMcon_sub, file="TNI_SelectVariables_SHBtoOPM_SD_Con.pdf", width=30, height=20, units="cm", dpi=300)
+
